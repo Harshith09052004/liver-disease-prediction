cccf6038f75777a153c80b5ad9eef10680708590
Harshith09052004 <podduturiharshith47@gmail.com>
2026-03-06 19:58:20 +0530

    Rename ClientSiteConfig to RemoteUserConfig

--- liver_disease_prediction/Remote_User/apps.py
+++ liver_disease_prediction/Remote_User/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
 
-class ClientSiteConfig(AppConfig):
+
+class RemoteUserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'liver_disease_prediction.Remote_User'
