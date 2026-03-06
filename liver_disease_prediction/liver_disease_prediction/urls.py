78a0e84b31804593b23c8f5dfb2cb551c2ab4cf0
Harshith09052004 <podduturiharshith47@gmail.com>
2026-03-06 19:54:35 +0530

    Refactor URL patterns for remote user and service provider

--- liver_disease_prediction/liver_disease_prediction/urls.py
+++ liver_disease_prediction/liver_disease_prediction/urls.py
@@ -8,26 +8,85 @@ from liver_disease_prediction.Service_Provider import views as serviceprovider
 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
 
-    # Remote user
+    # ---------------- REMOTE USER ----------------
     path('', remoteuser.login, name="login"),
+
     path('Register1/', remoteuser.Register1, name="Register1"),
-    path('Predict_Liver_Disease_Status/', remoteuser.Predict_Liver_Disease_Status, name="Predict_Liver_Disease_Status"),
-    path('ViewYourProfile/', remoteuser.ViewYourProfile, name="ViewYourProfile"),
 
-    # Service provider
-    path('serviceproviderlogin/', serviceprovider.serviceproviderlogin, name="serviceproviderlogin"),
-    path('View_Remote_Users/', serviceprovider.View_Remote_Users, name="View_Remote_Users"),
+    path(
+        'Predict_Liver_Disease_Status/',
+        remoteuser.Predict_Liver_Disease_Status,
+        name="Predict_Liver_Disease_Status"
+    ),
 
-    re_path(r'^charts/(?P<chart_type>\w+)/$', serviceprovider.charts, name="charts"),
-    re_path(r'^charts1/(?P<chart_type>\w+)/$', serviceprovider.charts1, name="charts1"),
-    re_path(r'^likeschart/(?P<like_chart>\w+)/$', serviceprovider.likeschart, name="likeschart"),
+    path(
+        'ViewYourProfile/',
+        remoteuser.ViewYourProfile,
+        name="ViewYourProfile"
+    ),
 
-    path('Find_Liver_Disease_Ratio/', serviceprovider.Find_Liver_Disease_Ratio, name="Find_Liver_Disease_Ratio"),
-    path('Train_Test_DataSets/', serviceprovider.Train_Test_DataSets, name="Train_Test_DataSets"),
-    path('View_Liver_Disease_Status/', serviceprovider.View_Liver_Disease_Status, name="View_Liver_Disease_Status"),
-    path('Download_Trained_DataSets/', serviceprovider.Download_Trained_DataSets, name="Download_Trained_DataSets"),
+
+    # ---------------- SERVICE PROVIDER ----------------
+    path(
+        'serviceproviderlogin/',
+        serviceprovider.serviceproviderlogin,
+        name="serviceproviderlogin"
+    ),
+
+    path(
+        'View_Remote_Users/',
+        serviceprovider.View_Remote_Users,
+        name="View_Remote_Users"
+    ),
+
+    path(
+        'Find_Liver_Disease_Ratio/',
+        serviceprovider.Find_Liver_Disease_Ratio,
+        name="Find_Liver_Disease_Ratio"
+    ),
+
+    path(
+        'Train_Test_DataSets/',
+        serviceprovider.Train_Test_DataSets,
+        name="Train_Test_DataSets"
+    ),
+
+    path(
+        'View_Liver_Disease_Status/',
+        serviceprovider.View_Liver_Disease_Status,
+        name="View_Liver_Disease_Status"
+    ),
+
+    path(
+        'Download_Trained_DataSets/',
+        serviceprovider.Download_Trained_DataSets,
+        name="Download_Trained_DataSets"
+    ),
+
+
+    # ---------------- CHARTS ----------------
+    re_path(
+        r'^charts/(?P<chart_type>\w+)/$',
+        serviceprovider.charts,
+        name="charts"
+    ),
+
+    re_path(
+        r'^charts1/(?P<chart_type>\w+)/$',
+        serviceprovider.charts1,
+        name="charts1"
+    ),
+
+    re_path(
+        r'^likeschart/(?P<like_chart>\w+)/$',
+        serviceprovider.likeschart,
+        name="likeschart"
+    ),
+
 ]
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
