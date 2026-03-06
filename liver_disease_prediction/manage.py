99c30f13f044ea3c4d6840af69fef8a6ba64c142
Harshith09052004 <podduturiharshith47@gmail.com>
2026-03-06 20:47:44 +0530

    Update DJANGO_SETTINGS_MODULE path in manage.py

--- liver_disease_prediction/manage.py
+++ liver_disease_prediction/manage.py
@@ -8,9 +8,9 @@ def main():
     """Run administrative tasks."""
 
     os.environ.setdefault(
-        'DJANGO_SETTINGS_MODULE',
-        'liver_disease_prediction.settings'
-    )
+    'DJANGO_SETTINGS_MODULE',
+    'liver_disease_prediction.liver_disease_prediction.settings'
+)
 
     try:
         from django.core.management import execute_from_command_line
