104dd089dd14f2a9a2e93b91900cc5caff1813c5
Harshith09052004 <podduturiharshith47@gmail.com>
2026-03-11 23:26:15 +0530

    Update wsgi.py

--- liver_disease_prediction/liver_disease_prediction/wsgi.py
+++ liver_disease_prediction/liver_disease_prediction/wsgi.py
@@ -1,10 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'liver_disease_prediction.liver_disease_prediction.settings'
-)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liver_disease_prediction.settings')
 
 application = get_wsgi_application()
