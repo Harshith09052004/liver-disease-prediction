b4a1351598bbc022fbbf1260498969e8f993da0e
Harshith09052004 <podduturiharshith47@gmail.com>
2026-03-12 00:33:50 +0530

    Update urls.py

--- liver_disease_prediction/liver_disease_prediction/urls.py
+++ liver_disease_prediction/liver_disease_prediction/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
 from Remote_User import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('predict/', views.predict),
+
+    path('', views.login, name='login'),
+    path('register/', views.Register1, name='register'),
+
+    path('profile/', views.ViewYourProfile, name='ViewYourProfile'),
+
+    path('predict/', views.Predict_Liver_Disease_Status,
+         name='Predict_Liver_Disease_Status'),
 ]
