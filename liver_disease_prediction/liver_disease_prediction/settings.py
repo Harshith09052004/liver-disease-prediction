da69b680c165b57ad032ade3878bdefaba21ab62
Harshith09052004 <podduturiharshith47@gmail.com>
2026-03-11 23:55:41 +0530

    Update settings.py

--- liver_disease_prediction/liver_disease_prediction/settings.py
+++ liver_disease_prediction/liver_disease_prediction/settings.py
@@ -16,6 +16,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'Remote_User',
+    'Service_Provider',
 ]
 
 MIDDLEWARE = [
