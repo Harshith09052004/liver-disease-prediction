fd6f3d489e3b48c53d0b978da6ed1452a5a29bab
Harshith Podduturi <podduturiharshith47@example.com>
2026-03-18 17:59:17 +0530

    debug on

--- liver_disease_prediction/liver_disease_prediction/settings.py
+++ liver_disease_prediction/liver_disease_prediction/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = 'your-secret-key'  # keep this same or change later
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # change later to your domain
 
